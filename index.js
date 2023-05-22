78af0ffdf55209fb4e3d10fbd62687a8785f4b2e
fernandolopez <lopez.fernandomartin98@gmail.com>
2023-05-22 15:01:03 -0300

    antes de la entrega

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // primero pedir monto total a calcular en cuotas 
-/*
 let montoTotal = 0
-
+//Nos aseguramos que sea un valor correcto y posible a calcular
 while (true) {
   montoTotal = parseInt(prompt("Ingrese el monto total"));
   if (!isNaN(montoTotal) && montoTotal != null && montoTotal != " ") {
@@ -12,16 +11,16 @@ while (true) {
   }
 }
 
-// mostramos el monto total ingresado por el usuario en pantalla 
+// Mostramos el monto total ingresado por el usuario en pantalla 
 
 alert("Monto total ingresado a calcular es: $" + montoTotal + "")
 console.log("Monto total ingresado a calcular es: $" + montoTotal + "")
 
-// le recordamos de sus posibilidades de financiacion 
+// Le recordamos de sus posibilidades de financiacion en cuotas sin interes
 
 alert("Recuerde que tiene 6 y 12 cuotas sin interes")
 
-// le pedimos que elija la cantidad de cuotas 
+// le pedimos que elija la cantidad de cuotas y nos aseguramos que sea dentro de los valores posibles
 
 let cantidadCuotas = 0
 while (true) {
@@ -33,9 +32,10 @@ while (true) {
     continue;
   }
 }
+//TASA DE INTERES 
+let tasaInteres = 1.05;
 
-const tasaInteres = 1.05;
-
+//funcion para calcular cuotas que muestra en pantalla solo el monto a pagar por mes
 function calcularCuotas(){
   let cuota = montoTotal/cantidadCuotas;
   for(let i=0; i < cantidadCuotas; i++){
@@ -47,8 +47,6 @@ function calcularCuotas(){
   }
 }
 calcularCuotas();
-
-*/
 
 class Indumentaria {
    constructor(id, nombre, precio, talles, colores, categoria) {
@@ -92,29 +90,45 @@ const arrayIndum = [
    remeraWhite,
 ];
 
+//mmostramos en pantalla los productos que tenemos en stock
 alert("Estos son nuestros productos en stock");
-arrayIndum.forEach((element)=> {
-alert(`${element.nombre} precio: $ ${element.precio} talles: ${element.talles} colores:  ${element.colores}`)
+arrayIndum.forEach(e => {
+   alert(`- ${e.nombre}: \n* Precio: $ ${e.precio} \n* Talles: ${e.talles}\n* Colores: ${e.colores}`) 
 })
 
+//le pedimos al usuario un numero para poder checkear el stock disponible
 let stockUser = parseInt(prompt("Ingrese un numero del 1 al 10"));
 if (stockUser < 5){
   alert("No hay prendas sin stock")
 }
-console.log(stockUser);
+console.log(`Numero ingresado por el usuario ${stockUser}`);
 
+// filtramos las prendas con poco stock oara mostrarlo en pantalla // POCO STOCK
 let stockMin = arrayIndum.filter((element)=> element.stock <= stockUser);
-stockMin.forEach((element) => alert(`Estos son los productos a comprar nuevamente: ${element.nombre}`));
+stockMin.forEach((element) => alert(`Estos son los productos a comprar nuevamente:\n ${element.nombre}`));
 
+// productos sin stock
 let sinStock = arrayIndum.filter((element) => element.stock == 0);
-console.log(sinStock);
+console.log(`Productos sin stock: ${sinStock}`);
 
+// ordenamos los productos de menor a mayor precio y lo mostramos por alerta
 arrayIndum.sort((a,b) => a.precio - b.precio);
-console.log(arrayIndum);
+console.log("Precios ordenados de menor a mayor :")
+console.table(arrayIndum);
 
 alert("Precios ordenados de menor a mayor")
 
-arrayIndum.forEach((element)=> {
-alert(`${element.nombre} precio: $ ${element.precio} talles: ${element.talles} colores:  ${element.colores}`)
+arrayIndum.forEach((e)=> {
+  alert(`- ${e.nombre}: \n- Precio: $ ${e.precio} \n- Talles: ${e.talles}\n- Colores: ${e.colores}`)  
 })
 
+//buscar por nombre del producto
+let productoBuscado = prompt("Ingrese el nombre del producto a buscar");
+
+console.log(productoBuscado);
+const productoEncontrado = arrayIndum.filter((e) => e.nombre.toLowerCase() == productoBuscado);
+productoEncontrado.forEach((e) => {
+  alert(`Resultado para la busqueda " ${productoBuscado} " :\n
+  - ${e.nombre}: \n- Precio: $ ${e.precio} \n- Talles: ${e.talles}\n- Colores: ${e.colores} `)
+})
+console.log(productoEncontrado);
