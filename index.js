0af4a4388c764cd568a0901feba70f6f2f5f9114
fernandolopez <lopez.fernandomartin98@gmail.com>
2023-06-12 20:26:44 -0300

    detalles y librerias

---
+++ index.js
@@ -0,0 +1,87 @@
+// primero pedir monto total a calcular en cuotas 
+let montoTotal = 0
+//Nos aseguramos que sea un valor correcto y posible a calcular
+while (true) {
+  montoTotal = parseInt(prompt("Ingrese el monto total"));
+  if (!isNaN(montoTotal) && montoTotal != null && montoTotal != " ") {
+    break;
+  } else {
+    alert("Monto ingresado incorrectamente, vuelva a intentarlo por favor");
+    continue;
+  }
+}
+
+// Mostramos el monto total ingresado por el usuario en pantalla 
+
+alert("Monto total ingresado a calcular es: $" + montoTotal + "")
+console.log("Monto total ingresado a calcular es: $" + montoTotal + "")
+
+// Le recordamos de sus posibilidades de financiacion en cuotas sin interes
+
+alert("Recuerde que tiene 6 y 12 cuotas sin interes")
+
+// le pedimos que elija la cantidad de cuotas y nos aseguramos que sea dentro de los valores posibles
+
+let cantidadCuotas = 0
+while (true) {
+  cantidadCuotas = parseInt(prompt("Elija la cantidad de cuotas"));
+  if (!isNaN(cantidadCuotas) && cantidadCuotas != null && cantidadCuotas != " ") {
+    break;
+  } else {
+    alert("Vuelva a intentarlo por favor");
+    continue;
+  }
+}
+//TASA DE INTERES 
+let tasaInteres = 1.05;
+
+//funcion para calcular cuotas que muestra en pantalla solo el monto a pagar por mes
+function calcularCuotas(){
+  let cuota = montoTotal/cantidadCuotas;
+  for(let i=0; i < cantidadCuotas; i++){
+    if(cantidadCuotas != 1 && cantidadCuotas != 6 && cantidadCuotas != 12){
+    cuota = (montoTotal/cantidadCuotas)* tasaInteres;
+  }
+  alert("Tu monto a pagar por mes $" + cuota.toFixed()+ "");
+  break;
+  }
+}
+calcularCuotas();
+
+
+// filtramos las prendas con poco stock oara mostrarlo en pantalla // POCO STOCK
+let stockMin = arrayIndum.filter((element)=> element.stock <= 3);
+stockMin.forEach((element) => alert(`Estos son los productos a comprar nuevamente:\n ${element.nombre}`));
+
+// productos sin stock
+let sinStock = arrayIndum.filter((element) => element.stock == 0);
+console.log(`Productos sin stock: ${sinStock}`);
+
+// ordenamos los productos de menor a mayor precio y lo mostramos por alerta
+arrayIndum.sort((a,b) => a.precio - b.precio);
+console.log("Precios ordenados de menor a mayor :")
+console.table(arrayIndum);
+
+alert("Precios ordenados de menor a mayor")
+
+arrayIndum.forEach((e)=> {
+  alert(`- ${e.nombre}: \n- Precio: $ ${e.precio} \n- Talles: ${e.talles}`)  
+})
+
+//buscar por nombre del producto
+
+const productoEncontrado = arrayIndum.filter((e) => e.nombre.toLowerCase() == productoBuscado);
+productoEncontrado.forEach((e) => {
+  alert(`Resultado para la busqueda " ${productoBuscado} " :\n
+  - ${e.nombre}: \n- Precio: $ ${e.precio} \n- Talles: ${e.talles} `)
+})
+console.log(productoEncontrado);
+
+// <!-- Luxon CDN -->
+//     <script src="https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js"></script>
+
+//     <!-- Toastify CDN -->
+//     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    
+//     <!-- SweetAlert CDN -->
+//     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
--- indexnuevo.js
+++ indexnuevo.js
@@ -1,49 +1,157 @@
 //stock disponible constructor para desarrollarlo 
 class Indumentaria {
-    constructor(id, nombre,img, precio, talles, categoria) {
-       this.id = id;
-       this.nombre = nombre;
-       this.img = img;
-       this.precio = parseFloat(precio);
-       this.talles = talles;
-       this.categoria = categoria;
-       this.stock = 5;
-    }
-    restaStock() {
-       this.stock = this.stock - 1;
-       }
-    sumarIva() {
-       return this.precio * 1.21;
-    }
-    precioSugerido() {
-       return this.precio * 1.21 * 1.25;
-    }
- }
- 
- const camperaDeJean = new Indumentaria(1, "Campera de jean",`./img/camperajean.png`, 15000, "S, M, L, XL", "camperas");
- const camperaCuero = new Indumentaria(2, "Campera de cuero",`./img/images.jpg`, 25000, "S, M, L, XL", "camperas");
- const pantalonJean = new Indumentaria(3, "Pantalon de jean",`./img/pantalonjean.jpg`, 10516, "38, 40, 42, 44", "pantalones");
- const pantalonJoggerMom = new Indumentaria(4, "Pantalon Jogger MOM",`./img/patanlonjoggermom.jpg`, 8200, "38, 40, 42, 44", "pantalones");
- const remeraDark = new Indumentaria(5, "Remera Dark Oversize",`./img/remeradark.jpg`, 7500, "S, M, L, XL", "remeras");
- const remeraWhite = new Indumentaria(6, "Remera White Oversize",`./img/remerawhite.jpg`, 7500, "S, M, L, XL", "remeras");
- const buzoOversize = new Indumentaria(7, "Buzo oversize",`./img/buzooversize.jpg`, 8400, "S, M, L, XL", "remeras");
- const buzoCanguro = new Indumentaria(8, "Buzo Canguro",`./img/buzocanguro.jpg`, 8800, "S, M, L, XL", "remeras");
- 
- const arrayIndum = [
-    camperaDeJean, 
-    camperaCuero, 
-    buzoOversize, 
-    buzoCanguro, 
-    pantalonJean, 
-    pantalonJoggerMom, 
-    remeraDark, 
-    remeraWhite,
- ];
- 
+   constructor(id, nombre, img, precio, talles, categoria) {
+      this.id = id;
+      this.nombre = nombre;
+      this.img = img;
+      this.precio = parseFloat(precio);
+      this.talles = talles;
+      this.categoria = categoria;
+      this.stock = 5;
+   }
+   restaStock() {
+      this.stock = this.stock - 1;
+   }
+   sumarIva() {
+      return this.precio * 1.21;
+   }
+   precioSugerido() {
+      return this.precio * 1.21 * 1.25;
+   }
+}
+
+const camperaDeJean = new Indumentaria(1, "Campera de jean", `./img/camperajean.png`, 15000, ["S", "M", "L", "XL"], "camperas");
+const camperaCuero = new Indumentaria(2, "Campera de cuero", `./img/images.jpg`, 25000, ["S", "M", "L", "XL"], "camperas");
+const pantalonJean = new Indumentaria(3, "Pantalon de jean", `./img/pantalonjean.jpg`, 10516, ["38", "40", "42", "44"], "pantalones");
+const pantalonJoggerMom = new Indumentaria(4, "Pantalon Jogger MOM", `./img/patanlonjoggermom.jpg`, 8200, ["38", "40", "42", "44"], "pantalones");
+const remeraDark = new Indumentaria(5, "Remera Dark Oversize", `./img/remeradark.jpg`, 7500, ["S", "M", "L", "XL"], "remeras");
+const remeraWhite = new Indumentaria(6, "Remera White Oversize", `./img/remerawhite.jpg`, 7500, ["S", "M", "L", "XL"], "remeras");
+const buzoOversize = new Indumentaria(7, "Buzo oversize", `./img/buzooversize.jpg`, 8400, ["S", "M", "L", "XL"], "buzos");
+const buzoCanguro = new Indumentaria(8, "Buzo Canguro", `./img/buzocanguro.jpg`, 8800, ["S", "M", "L", "XL"], "buzos");
+
+const arrayIndum = [
+   camperaDeJean,
+   camperaCuero,
+   buzoOversize,
+   buzoCanguro,
+   pantalonJean,
+   pantalonJoggerMom,
+   remeraDark,
+   remeraWhite,
+];
+
 
 const indumProductos = document.getElementById("indumProductos")
-const carritoContenedor = document.getElementById("carrito-contain") 
+const carritoContenedor = document.getElementById("carrito-contain")
 const precioTotal = document.getElementById("precioTotal")
+const contadorCarrito = document.getElementById("contadorCarrito")
+
+// recorremos el array y creamos una card por cada producto
+const renderizar = () => {
+   arrayIndum.forEach((e => {
+      let divCard = document.createElement("div")
+
+      divCard.innerHTML = `
+     <div class="card carta" style="width: 18rem;">
+     <img src="${e.img}" class="card-img-top img-css" alt="${e.img}">
+   <div class="card-body">
+     <h5 class="card-title nombreProd">${e.nombre}</h5>
+   </div>
+   <ul class="list-group list-group-flush">
+     <li class="list-group-item">Precio: $${e.precio}</li>
+     <li class="list-group-item">
+     <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+     <option selected>Elegí tu talle</option>
+     <option value="1">${e.talles[0]}</option>
+     <option value="2">${e.talles[1]}</option>
+     <option value="3">${e.talles[2]}</option>
+     <option value="4">${e.talles[3]}</option>
+     </select></li>
+   </ul>
+   <div class="card-body d-flex justify-content-center">
+   <input type="button" class="btn" value="Agregar al carrito" id="agregar${e.id}"></a>
+   </div>
+ </div>
+     `
+      indumProductos.append(divCard)
+
+      const btnAgregar = document.getElementById(`agregar${e.id}`)
+
+      btnAgregar.addEventListener(`click`, () => {
+         agregarAlCarrito(e.id)
+         Swal.fire({
+            position: 'bottom-end',
+            showCancelButton:false,
+            showClass: {
+               popup: 'animate__animated animate__fadeInDown'
+             },
+             hideClass: {
+               popup: 'animate__animated animate__fadeOutUp'
+             },
+            icon: 'success',
+            html:
+            `Agregaste <b>${e.nombre}</b> al carrito`, 
+            showConfirmButton: false,
+            timer: 3000
+         })
+      })
+   }))
+}
+renderizar();
+
+// creamos un array vacio para el carrito
+let carrito = [];
+
+
+
+//local storage del carrito
+document.addEventListener(`DOMContentLoaded`, () => {
+   if (localStorage.getItem(`carrito`)) {
+      carrito = JSON.parse(localStorage.getItem(`carrito`))
+      actCarrito()
+   }
+})
+
+//funcion para agregar al carrito
+
+const agregarAlCarrito = (indumId) => {
+   const item = arrayIndum.find((indum) => indum.id === indumId)
+   carrito.push(item)
+   actCarrito()
+}
+
+//funcion para eliminar del carrito 
+
+const eliminarDelCarrito = (indumId) => {
+   const item = carrito.find((indum) => indum.id === indumId)
+   const indice = carrito.indexOf(item)
+   carrito.splice(indice, 1)
+   actCarrito()
+}
+
+//funcion para actualizar carrito 
+
+const actCarrito = () => {
+
+   carritoContenedor.innerHTML = ""
+
+   carrito.forEach((prod) => {
+      const div = document.createElement(`div`)
+      div.className = (`prodEnCarrito`)
+      div.innerHTML = `
+      <p>${prod.nombre}</p>
+      <p>Precio: $${prod.precio}</p>
+      <button value="X" onclick = "eliminarDelCarrito (${prod.id})"><i class="bi bi-trash"></i></button>
+      `
+
+      carritoContenedor.appendChild(div)
+
+      localStorage.setItem(`carrito`, JSON.stringify(carrito))
+   })
+   precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+   contadorCarrito.innerText = carrito.length
+}
+
 
 
 const prodBuscado = (e) => {
@@ -56,10 +164,10 @@ const prodBuscado = (e) => {
       return producto.nombre.toLowerCase().includes(valor.toLowerCase());
    });
    console.log(prodEncontrado)
-   prodEncontrado.forEach(( e => { 
-  
+   prodEncontrado.forEach((e => {
+
       let divCard = document.createElement("div")
- 
+
       divCard.innerHTML = `
       <div class="card carta" style="width: 18rem;">
       <img src="${e.img}" class="card-img-top img-css" alt="${e.img}">
@@ -68,101 +176,137 @@ const prodBuscado = (e) => {
     </div>
     <ul class="list-group list-group-flush">
       <li class="list-group-item">Precio: $${e.precio}</li>
-      <li class="list-group-item">Talles: ${e.talles}</li>
-    </ul>
+      <li class="list-group-item">
+      <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+      <option selected>Elegí tu talle</option>
+      <option value="1">${e.talles[0]}</option>
+      <option value="2">${e.talles[1]}</option>
+      <option value="3">${e.talles[2]}</option>
+      <option value="4">${e.talles[3]}</option>
+      </select></li>
     <div class="card-body d-flex justify-content-center">
-    <input type="button" class="btn" value="Agregar" id="agregar${e.id}"></a>
+    <input type="button" class="btn" value="Agregar al carrito" id="agregar${e.id}"></a>
     </div>
   </div>
       `
       indumProductos.append(divCard)
       const btnAgregar = document.getElementById(`agregar${e.id}`)
       btnAgregar.addEventListener(`click`, () => {
-       agregarAlCarrito(e.id)
+         agregarAlCarrito(e.id)
+         Swal.fire({
+            position: 'bottom-end',
+            showCancelButton:false,
+            showClass: {
+               popup: 'animate__animated animate__fadeInDown'
+             },
+             hideClass: {
+               popup: 'animate__animated animate__fadeOutUp'
+             },
+            icon: 'success',
+            html:
+            `Agregaste <b>${e.nombre}</b> al carrito`,
+            showConfirmButton: false,
+            timer: 3000,
+         })
       })
- }))
+   }))
 }
 const formBusqueda = document.getElementById("formBusqueda")
-formBusqueda.addEventListener("submit", (e) =>{
+formBusqueda.addEventListener("submit", (e) => {
    prodBuscado(e)
 })
 
-// recorremos el array y creamos una card por cada producto
-const renderizar = () => {
-   arrayIndum.forEach(( e => { 
-     let divCard = document.createElement("div")
- 
-     divCard.innerHTML = `
-     <div class="card carta" style="width: 18rem;">
-     <img src="${e.img}" class="card-img-top img-css" alt="${e.img}">
-   <div class="card-body">
-     <h5 class="card-title nombreProd">${e.nombre}</h5>
-   </div>
-   <ul class="list-group list-group-flush">
-     <li class="list-group-item">Precio: $${e.precio}</li>
-     <li class="list-group-item">Talles: ${e.talles}</li>
-   </ul>
-   <div class="card-body d-flex justify-content-center">
-   <input type="button" class="btn" value="Agregar" id="agregar${e.id}"></a>
-   </div>
+const filtrarCategorias = (categoria) => {
+   indumProductos.innerHTML = ""
+   const catEncontrada = arrayIndum.filter((prod) => prod.categoria.toLowerCase() === categoria);
+   console.log(catEncontrada);
+
+   catEncontrada.forEach((e => {
+
+      let divCard = document.createElement("div")
+
+      divCard.innerHTML = `
+   <div class="card carta" style="width: 18rem;">
+   <img src="${e.img}" class="card-img-top img-css" alt="${e.img}">
+ <div class="card-body">
+   <h5 class="card-title nombreProd">${e.nombre}</h5>
  </div>
-     `
-     indumProductos.append(divCard)
-
-     const btnAgregar = document.getElementById(`agregar${e.id}`)
-
-     btnAgregar.addEventListener(`click`, () => {
-      agregarAlCarrito(e.id)
-     })
-}))
+ <ul class="list-group list-group-flush">
+   <li class="list-group-item">Precio: $${e.precio}</li>
+   <li class="list-group-item">
+   <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+   <option selected>Elegí tu talle</option>
+   <option value="1">${e.talles[0]}</option>
+   <option value="2">${e.talles[1]}</option>
+   <option value="3">${e.talles[2]}</option>
+   <option value="4">${e.talles[3]}</option>
+   </select></li>
+ <div class="card-body d-flex justify-content-center">
+ <input type="button" class="btn" value="Agregar al carrito" id="agregar${e.id}"></a>
+ </div>
+</div>
+   `
+      indumProductos.append(divCard)
+      const btnAgregar = document.getElementById(`agregar${e.id}`)
+      btnAgregar.addEventListener(`click`, () => {
+         agregarAlCarrito(e.id)
+         Swal.fire({
+            position: 'bottom-end',
+            showCancelButton:false,
+            showClass: {
+               popup: 'animate__animated animate__fadeInDown'
+             },
+             hideClass: {
+               popup: 'animate__animated animate__fadeOutUp'
+             },
+            icon: 'success',
+            html:
+            `Agregaste <b>${e.nombre}</b> al carrito`,
+            showConfirmButton: false,
+            timer: 3000
+         })
+      })
+   }))
 }
-renderizar();
 
-// creamos un array vacio para el carrito
-let carrito = [];
-
-document.addEventListener(`DOMContentLoaded`, () => {
-   if(localStorage.getItem(`carrito`)){
-      carrito = JSON.parse(localStorage.getItem(`carrito`))
-      actCarrito()
-   }
+const catCamperas = document.getElementById("cat-camperas")
+catCamperas.addEventListener("click", () => {
+   filtrarCategorias("camperas")
 })
 
-//funcion para agregar al carrito
+const catBuzos = document.getElementById("cat-buzos")
+catBuzos.addEventListener("click", () => {
+   filtrarCategorias("buzos")
+})
 
-const agregarAlCarrito = (indumId) => {
-     const item = arrayIndum.find((indum) => indum.id === indumId)
-     carrito.push(item)
-     actCarrito()
-}
+const catPantalones = document.getElementById("cat-pantalones")
+catPantalones.addEventListener("click", () => {
+   filtrarCategorias("pantalones")
+})
 
-//funcion para eliminar del carrito 
+const catRemeras = document.getElementById("cat-remeras")
+catRemeras.addEventListener("click", () => {
+   filtrarCategorias("remeras")
+})
 
-const eliminarDelCarrito = (indumId) => {
-   const item = carrito.find((indum) => indum.id === indumId)
-   const indice = carrito.indexOf(item)
-   carrito.splice(indice,1)
-   actCarrito()
-}
+const home = document.getElementById("home")
+home.addEventListener(`click`, () => {
+   location.reload()
+})
 
-//funcion para actualizar carrito 
+//filtro para ordenar los precios en forma ascendente 
+const filtroPreciosAsc = () => arrayIndum.sort((a, b) => a.precio - b.precio)
+console.log(filtroPreciosAsc())
+const filtroPreciosDes = () => arrayIndum.sort((a, b) => b.precio - a.precio)
+console.log(filtroPreciosDes())
 
-const actCarrito = () => {
 
-    carritoContenedor.innerHTML = ""
-
-   carrito.forEach((prod) => {
-      const div = document.createElement(`div`)
-      div.className = (`prodEnCarrito`)
-      div.innerHTML = `
-      <p>${prod.nombre}</p>
-      <p>Precio: $${prod.precio}</p>
-      <button value="X" onclick = "eliminarDelCarrito (${prod.id})" class = "btn-css"></button>
-      `
-
-      carritoContenedor.appendChild(div)
-
-      localStorage.setItem(`carrito`, JSON.stringify(carrito))
-   })
-   precioTotal.innerText = carrito.reduce((acc,prod) => acc + prod.precio,0)
-}
+const filtrito = document.getElementById("filtrito");
+filtrito.addEventListener(`change`, (e) => {
+   let valorOption = e.target.value;
+   if (valorOption == "ascendente") {
+      console.log(filtroPreciosAsc())
+   } else if (valorOption == "descendente") {
+      console.log(filtroPreciosDes())
+   }
+})
